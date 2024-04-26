39f06a364ea1a157fcb948202a9b16b1c8496dca
Art Mag Fhionnghaile <123363826+S00237994@users.noreply.github.com>
2024-04-26 17:32:52 +0100

    added code

---
+++ SQTAssignment (1)/SQTAssignment/SQTAssignment/SQTAssignment_Tests/Program.cs
@@ -0,0 +1,125 @@
+﻿using NUnit.Framework;
+using Moq;
+using SQTAssignment_Tests;
+using SQTAssignment;
+
+
+namespace SQTAssignment_Tests
+{
+    [TestFixture]
+    internal class Program
+    {
+        [Test]
+        public static void Test1()
+        {
+            //SQTAssignment.InsuranceService myClass = new SQTAssignment.InsuranceService();
+
+
+            // Arrange
+            var mockDiscountService = new Mock<IDiscountService>();
+            mockDiscountService.Setup(x => x.GetDiscount()).Returns(0);
+            InsuranceService insurance = new InsuranceService(mockDiscountService.Object);
+
+            // Act
+            double result = insurance.CalcPremium(20, "rural");
+
+            // Assert
+            //Assert.AreEqual("It's hot!", wea`therDescription); // classic
+            Assert.That(result, Is.EqualTo(5)); // constraint
+
+        }
+
+
+        [Test]
+        public static void Test2()
+        {
+
+
+            // Arrange
+            var mockDiscountService = new Mock<IDiscountService>();
+            mockDiscountService.Setup(x => x.GetDiscount()).Returns(0);
+            InsuranceService insurance = new InsuranceService(mockDiscountService.Object);
+
+            // Act
+            double result = insurance.CalcPremium(16, "rural");
+
+            // Assert
+
+            Assert.That(result, Is.EqualTo(0)); // constraint
+
+        }
+
+        [Test]
+        public static void Test3()
+        {
+
+
+            // Arrange
+            var mockDiscountService = new Mock<IDiscountService>();
+            mockDiscountService.Setup(x => x.GetDiscount()).Returns(0);
+            InsuranceService insurance = new InsuranceService(mockDiscountService.Object);
+
+            // Act
+            double result = insurance.CalcPremium(32, "rural");
+
+            // Assert
+
+            Assert.That(result, Is.EqualTo(2.5)); // constraint
+
+        }
+        [Test]
+        public static void Test4()
+        {
+
+
+            // Arrange
+            var mockDiscountService = new Mock<IDiscountService>();
+            mockDiscountService.Setup(x => x.GetDiscount()).Returns(0);
+            InsuranceService insurance = new InsuranceService(mockDiscountService.Object);
+
+            // Act
+            double result = insurance.CalcPremium(21, "urban");
+
+            // Assert
+
+            Assert.That(result, Is.EqualTo(6)); // constraint
+
+        }
+        [Test]
+        public static void Test5()
+        {
+
+
+            // Arrange
+            var mockDiscountService = new Mock<IDiscountService>();
+            mockDiscountService.Setup(x => x.GetDiscount()).Returns(0);
+            InsuranceService insurance = new InsuranceService(mockDiscountService.Object);
+
+            // Act
+            double result = insurance.CalcPremium(17, "urban");
+
+            // Assert
+
+            Assert.That(result, Is.EqualTo(0)); // constraint
+
+        }
+        [Test]
+        public static void Test6()
+        {
+
+
+            // Arrange
+            var mockDiscountService = new Mock<IDiscountService>();
+            mockDiscountService.Setup(x => x.GetDiscount()).Returns(0);
+            InsuranceService insurance = new InsuranceService(mockDiscountService.Object);
+
+            // Act
+            double result = insurance.CalcPremium(37, "urban");
+
+            // Assert
+
+            Assert.That(result, Is.EqualTo(5)); // constraint
+
+        }
+    }
+}
